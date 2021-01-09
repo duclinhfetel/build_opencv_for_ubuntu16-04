617baddc71184676b07b9f88bf90ed620c416398
Linh Nguyen <duclinh.fetel@gmail.com>
2021-01-09 19:46:47 +0700

    Add files via upload

---
+++ install_opencv_ubuntu18-04.sh
@@ -0,0 +1,35 @@
+sudo apt-get update 
+sudo apt-get install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev -y
+cd 
+git clone https://github.com/opencv/opencv.git
+cd opencv 
+git checkout 4.5.1 
+cd
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 4.5.1
+cd
+cd opencv
+mkdir build && cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_C_EXAMPLES=ON \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=ON \
+      -D WITH_OPENGL=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_EXAMPLES=ON ..
+#make -j$(nproc)
+#sudo make install
+#sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+#sudo ldconfig
+#echo "========================================"
+#echo "Finshed Install Opencv (C++ and Python)"
+#echo "========================================"
